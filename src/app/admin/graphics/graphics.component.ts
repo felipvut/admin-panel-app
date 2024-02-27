dad2a2d1adaa47f59466e522e4136fbd67a94b53
Felipe Ferreira <felipvut@gmail.com>
2024-02-27 20:05:51 -0300

    Adicionando primeira parte do projeto

--- src/app/admin/graphics/graphics.component.ts
+++ src/app/admin/graphics/graphics.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-graphics',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
   templateUrl: './graphics.component.html',
   styleUrl: './graphics.component.scss'
 })
-export class GraphicsComponent {
+export class GraphicsComponent implements OnInit{
+  
+  public chart: any;
 
+  ngOnInit(): void {
+    this.createChart()
+  }
+
+  createChart(){
+    this.chart = new Chart("MyChart", {
+      type: 'line', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
+								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
+	       datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92',
+								 '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: ['542', '542', '536', '327', '17',
+									 '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          }  
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+      
+    });
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule, RouterOutlet, MatToolbarModule,
-    MatIconModule, MatButtonModule, MatSidenavModule
+    MatIconModule, MatButtonModule, MatSidenavModule,
+    RouterModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'admin-panel-app';
   open: boolean = true;
-  lightMode: Boolean = false;
+  lightMode: Boolean = true;
 
+  constructor(
+    protected route: Router
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    let value = localStorage.getItem("isLight")
+    if(value == "true") {
+      this.lightMode = true
+    } else if(value == "false") {
+      this.lightMode = false
+    } else {
+      this.lightMode = true
+    }
+    this.setMode(true)
+  }
+  
   sideBarItens: any[] = [
-    {title: "Gráficos", materialIconName: "bar_chart"},
+    {title: "Gráficos", materialIconName: "bar_chart", redirect: "/graphics"},
     {title: "Funcionários", materialIconName: "person_pin"},
     {title: "Clientes", materialIconName: "person"},
     {title: "Configurações", materialIconName: "settings"},
     {title: "Sair", materialIconName: "exit_to_app"}
   ]
 
-  setMode() {
-    this.lightMode = !this.lightMode
+  setMode(init: boolean = false){
+    if(!init) {
+      this.lightMode = !this.lightMode
+    } 
+    localStorage.setItem("isLight", this.lightMode.toString())
     if(!this.lightMode) {
-      document.body.style.setProperty('--primary', '#0f0f11');
-      document.body.style.setProperty('--secundary', '#232125');
-      document.body.style.setProperty('--terciary', '#232125');
+      document.body.style.setProperty('--primary', '#101725');
+      document.body.style.setProperty('--secundary', '#202837');
+      document.body.style.setProperty('--terciary', '#384152');
       document.body.style.setProperty('--text-color', '#fff');
     } else {
       document.body.style.setProperty('--primary', '#fff');
-      document.body.style.setProperty('--secundary', '#000');
-      document.body.style.setProperty('--terciary', '#fff');
       document.body.style.setProperty('--text-color', '#000');
     }
   }
